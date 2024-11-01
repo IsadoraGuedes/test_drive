85440d2e7e92d01139db9f98428c0c3d44c0d7a4
Isadora Guedes <isasoaresguedes@gmail.com>
2024-11-01 00:40:31 -0300

    Adiciona funcionamento do image_picker

---
+++ lib/pose_picker.dart
@@ -0,0 +1,238 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mime/mime.dart';
+import 'package:test_drive/main.dart';
+
+class PosePicker extends StatefulWidget {
+  const PosePicker({super.key});
+
+  @override
+  State<PosePicker> createState() => _PosePageState();
+}
+
+class _PosePageState extends State<PosePicker> {
+  List<XFile>? _mediaFileList;
+
+  void _setImageFileListFromFile(XFile? value) {
+    _mediaFileList = value == null ? null : <XFile>[value];
+  }
+
+  dynamic _pickImageError;
+  bool isVideo = false;
+
+  String? _retrieveDataError;
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _onImageButtonPressed(
+    ImageSource source, {
+    required BuildContext context,
+  }) async {
+    if (context.mounted) {
+      await _displayPickImageDialog(context, false, (double? maxWidth,
+          double? maxHeight, int? quality, int? limit) async {
+        try {
+          final XFile? pickedFile = await _picker.pickImage(
+            source: source,
+            maxWidth: maxWidth,
+            maxHeight: maxHeight,
+            imageQuality: quality,
+          );
+          setState(() {
+            _setImageFileListFromFile(pickedFile);
+          });
+        } catch (e) {
+          setState(() {
+            _pickImageError = e;
+          });
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    print(' dispose ');
+    super.dispose();
+  }
+
+  Widget _previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_mediaFileList != null) {
+      return Semantics(
+        label: 'image_picker_example_picked_images',
+        child: ListView.builder(
+          key: UniqueKey(),
+          itemBuilder: (BuildContext context, int index) {
+            final String? mime = lookupMimeType(_mediaFileList![index].path);
+
+            // Why network for web?
+            // See https://pub.dev/packages/image_picker_for_web#limitations-on-the-web-platform
+            return Semantics(
+              label: 'image_picker_example_picked_image',
+              child: kIsWeb
+                  ? Image.network(_mediaFileList![index].path)
+                  : (mime == null || mime.startsWith('image/')
+                      ? Image.file(
+                          File(_mediaFileList![index].path),
+                          errorBuilder: (BuildContext context, Object error,
+                              StackTrace? stackTrace) {
+                            return const Center(
+                                child:
+                                    Text('This image type is not supported'));
+                          },
+                        )
+                      : null),
+            );
+          },
+          itemCount: _mediaFileList!.length,
+        ),
+      );
+    } else if (_pickImageError != null) {
+      return Text(
+        'Pick image error: $_pickImageError',
+        textAlign: TextAlign.center,
+      );
+    } else {
+      return getPageButtons();
+    }
+  }
+
+  Widget _handlePreview() {
+    return _previewImages();
+  }
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await _picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      isVideo = false;
+      setState(() {
+        if (response.files == null) {
+          _setImageFileListFromFile(response.file);
+        } else {
+          _mediaFileList = response.files;
+        }
+      });
+    } else {
+      _retrieveDataError = response.exception!.code;
+    }
+  }
+
+  Widget getPageButtons() {
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      const Text(
+        'Selecione a peça de roupa desejada',
+        style: TextStyle(fontSize: 18, color: Colors.black54),
+        textAlign: TextAlign.center,
+      ),
+      const SizedBox(height: 40),
+      CustomButton(
+        text: 'Carregar\nda galeria',
+        color: const Color(0xFF6B4C7A),
+        onPressed: () {
+          print(' press button 2');
+          _onImageButtonPressed(ImageSource.gallery, context: context);
+          // Handle "Carregar da galeria" button press
+        },
+      ),
+      const SizedBox(height: 20),
+      CustomButton(
+        text: 'Carregar\nda camera',
+        color: const Color(0xFF4C3F58),
+        onPressed: () {
+          _onImageButtonPressed(ImageSource.camera, context: context);
+        },
+      ),
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Peça de roupa'),
+        ),
+        body: Center(
+          child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
+              ? FutureBuilder<void>(
+                  future: retrieveLostData(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<void> snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        print('wating');
+                        return getPageButtons();
+                      case ConnectionState.done:
+                        return _handlePreview();
+                      case ConnectionState.active:
+                        print('acTIVE');
+                        if (snapshot.hasError) {
+                          return Text(
+                            'Pick image/video error: ${snapshot.error}}',
+                            textAlign: TextAlign.center,
+                          );
+                        } else {
+                          return getPageButtons();
+                        }
+                    }
+                  },
+                )
+              : _handlePreview(),
+        ));
+  }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
+  }
+
+  Future<void> _displayPickImageDialog(
+      BuildContext context, bool isMulti, OnPickImageCallback onPick) async {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Add optional parameters'),
+            content: const Text(' confirmar? '),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                  child: const Text('PICK'),
+                  onPressed: () {
+                    onPick(null, null, null, 1);
+                    Navigator.of(context).pop();
+                  }),
+            ],
+          );
+        });
+  }
+}
+
+typedef OnPickImageCallback = void Function(
+    double? maxWidth, double? maxHeight, int? quality, int? limit);
